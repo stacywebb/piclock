1182e17b1fb6d956b850bc6258121f043487934d
Stacy Webb <swebb@valcom.com>
2017-01-07 19:02:41 -0500

    Initial Commit

---
+++ Clock/ApiKeys-example.py
@@ -0,0 +1,5 @@
+# change this to your API keys
+# Weather Underground API key
+wuapi = '6cf9dca794440b3a'
+# Google Maps API key
+googleapi = 'AIzaSyDP6aVOddFzxzyipAItRJB0ocDs0zlplsU'   # Empty string, optional -- with PiClock, you'll be ok
